b67f4a708f48be345a9751cf36c01a5ef0c44811
ErmirMehaj <143171604+ErmirMehaj@users.noreply.github.com>
2025-09-19 15:18:20 +0200

    Update BallScript.cs
    La til lydfiler. Når du trykker "space" så kommer hoppelyden hvor hver gang du trykker. Når du treffer en pipe, så kommer kollisjonlyden

--- BallScript.cs
+++ BallScript.cs
@@ -11,6 +11,12 @@ public class BallScript : MonoBehaviour
     public LogicScript logic;
     // Holder styr på om ballen lever eller har dødd
     public bool ballIsAlive = true;
+    
+    //  AudioSource-komponenten som spiller av hoppelyden (lastet ned som .mp3 lydfyl og la i assets og dro den opp i inspectoren)
+    public AudioSoucre jumpSound;
+    // AudioSource-komponenten som spiller av kollisjonslyden 
+    public AudioSource collisionSound;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,6 +32,8 @@ public class BallScript : MonoBehaviour
         {
             // Ballen hopper oppver 
             myRigidbody.linearVelocity = Vector2.up * flapStrength;
+            // Når du trykker space, så kommer "jumpSound" lyden for hver trykk
+            jumpSound.PlayOneShot(jumpSound.clip);
         }
 
         
@@ -34,6 +42,8 @@ public class BallScript : MonoBehaviour
     // Kalles når ballen treffer noe som pipe
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Når ballen treffer pipen, så kommer kollisjon lyden til å spilles av
+        collisionSound.Play();
         // Kjører gameOver funksjonen i logic scriptet
         logic.gameOver();
         // Markerer at ballen ikke lever lenger
